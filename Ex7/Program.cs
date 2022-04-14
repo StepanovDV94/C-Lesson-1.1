acb82ada9d6b08774efcc1775a662dd12afff6c7
Dima <stepanovdmitriy94@mail.ru>
2022-04-14 18:54:26 +0300

    Add Ex7

---
+++ Ex7/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+
+Console.WriteLine(max);
