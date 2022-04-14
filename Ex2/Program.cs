50ac443a2cffb076fc7513b2fa6bbb812ee61d5e
Dima <stepanovdmitriy94@mail.ru>
2022-04-14 16:39:28 +0300

    Add Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a*a==b) 
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
