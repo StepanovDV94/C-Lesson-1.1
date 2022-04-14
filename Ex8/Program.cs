3a35e98b1f68478e88e5c3c51e6f0ee05e9a5f34
Dima <stepanovdmitriy94@mail.ru>
2022-04-14 19:00:40 +0300

    Add Ex8

---
+++ Ex8/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a==0) Console.WriteLine("Введите число отличное от нуля");
+else
+if (a%2==0) Console.WriteLine("Четное");
+else Console.WriteLine("Нечетное");
