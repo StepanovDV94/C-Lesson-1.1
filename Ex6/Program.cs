69bd8af2544feba15b4dcf3a75af4f87ceefdad2
Dima <stepanovdmitriy94@mail.ru>
2022-04-14 18:36:43 +0300

    Add Ex6

---
+++ Ex6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b) 
+{
+    Console.WriteLine("Первое число больше");
+}
+else if (a<b) 
+{
+    Console.WriteLine("Второе число больше");
+}
+else Console.WriteLine("Числа равны");
