08f4ce72264265e61ab6d8709dd9eb3ec1a32d24
Dima <stepanovdmitriy94@mail.ru>
2022-04-14 19:14:54 +0300

    Add Ex9

---
+++ Ex9/Program.cs
@@ -0,0 +1,15 @@
+﻿int a =0;
+
+while (a<=0)
+{
+Console.WriteLine("Введите число больше 0");
+a = Convert.ToInt32(Console.ReadLine());
+}
+
+int count = 0;
+
+while (count<=a)
+{
+    if (count%2==0) Console.WriteLine(count);
+    count+=1;
+}
