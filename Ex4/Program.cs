ad9a9ad493da23ed51028e0e653cae87a3778806
Dima <stepanovdmitriy94@mail.ru>
2022-04-14 18:04:14 +0300

    Add Ex4

---
+++ Ex4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int count = a*(-1);
+
+    while (count<=a)
+    {
+        if (count%2==0&&count!=0) Console.WriteLine(count);
+        count+=1;
+    }
+
